7d46ca0ea722eb4006d80a2e4db9833d5310ad4d
chatchate <130384596+chatchate@users.noreply.github.com>
2023-04-10 16:36:28 +0000

    deleted sth

--- src/components/Menu/MenuOptions/Me.tsx
+++ src/components/Menu/MenuOptions/Me.tsx
@@ -6,13 +6,7 @@ import HeartIcon from '@icon/HeartIcon';
 const Me = () => {
   const { t } = useTranslation();
   return (
-    <a
-      className='flex py-2 px-2 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm'
-      href='https://github.com/ztjhz/BetterChatGPT'
-      target='_blank'
-    >
-      <HeartIcon />
-      {t('author')}
+    <a>
     </a>
   );
 };
